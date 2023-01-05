db80a80610eb6da2bfaec900dc6cacd179fee00d
jhonpulido1990 <3825@holbertonstudents.com>
2023-01-05 17:59:10 -0500

    using logging

---
+++ built-in/builtin-2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logging.info('arrancando programa')
+logging.warning('hace calor')
+logging.error('test')
+logging.critical('adios')
