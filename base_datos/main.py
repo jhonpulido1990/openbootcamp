88a0ebc9b79e631c1269c8b8d167055f1ed2a662
jhonpulido1990 <3825@holbertonstudents.com>
2023-01-13 01:22:36 -0500

    fixed code

--- base_datos/main.py
+++ base_datos/main.py
@@ -27,9 +27,17 @@ def desconectarse(mi_conexion):
 
 def hacer_conasulta(mi_conexion):
     mi_cursor = mi_conexion.cursor()
-    mi_cursor.execute('select * from users;')
+    mi_cursor.execute("select * from Alumnos where users = 'paola';")
     for bd in mi_cursor:
         print(bd)
+    mi_cursor.close()
+
+def crear_user(mi_conexion, users, passwd):
+    mi_cursor = mi_conexion.cursor()
+    mi_cursor.execute('create table if not exists Alumnos (id int primary key, users varchar(255), passwd varchar(255));')
+    mi_cursor.execute('alter table Alumnos modify column id int AUTO_INCREMENT;')
+    mi_cursor.execute("insert into Alumnos (users, passwd) values (%s, %s);", (users, passwd))
+    mi_conexion.commit()
     mi_cursor.close()
 
 
@@ -38,5 +46,13 @@ if __name__ == '__main__':
     c = getpass.getpass('introduce contraseña: ')
     mi_conexion = conectarse(u, c)
     if mi_conexion:
+        crear_user(mi_conexion, 'caterine', 'dato1')
+        crear_user(mi_conexion, 'paola', 'dato2')
+        crear_user(mi_conexion, 'julia', 'dato3')
+        crear_user(mi_conexion, 'maria', 'dato4')
+        crear_user(mi_conexion, 'andrea', 'dato5')
+        crear_user(mi_conexion, 'catalina', 'dato6')
+        crear_user(mi_conexion, 'alexandra', 'dato7')
+        crear_user(mi_conexion, 'karolina', 'dato8')
         hacer_conasulta(mi_conexion)
         desconectarse(mi_conexion)
