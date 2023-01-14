e494e25385bffd5f9cfc4661c083cb22a1c9d311
jhonpulido1990 <3825@holbertonstudents.com>
2023-01-14 17:58:53 -0500

    add more advance

--- django/catalog/models.py
+++ django/catalog/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.urls import reverse
+from uuid import uuid4
 
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=64, help_text='pon el nombre del genero')
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=32)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=100, help_text='introduccion del libro')
+    isbn = models.CharField('ISBN', max_length=13, help_text='el ISBN de 13 caracteres')
+    genre = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, help_text='ID unico para cada libro')
+    book = models.ForeignKey('book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+
+    LOAN_STATUS = (
+        ('m', 'Maintenamce'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='disponibilidad del libro')
+
+    class META:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return '%s (%s)' % (self.id, self.book.title)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reversed('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return '%s, %s' % (self.last_name, self.first_name)
