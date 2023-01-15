e55ad9ec935df6dd30d7380df96eb22babc40eb7
jhonpulido1990 <3825@holbertonstudents.com>
2023-01-15 12:10:51 -0500

    end word django

--- django/catalog/views.py
+++ django/catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Book, BookInstance, Genre, Author
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_authors = Author.objects.all().count()
+
+    disponibles = BookInstance.objects.filter(status__exact='a').count()
+
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books': num_books,
+            'num_authors': num_authors,
+            'disponibles': disponibles,
+            'num_instances': num_instances
+        }
+    )
